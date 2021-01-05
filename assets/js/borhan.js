5df4964353001c07614e8ded5d255ff99ca03c23
borhan365 <info.mdborhan@gmail.com>
2021-01-05 23:26:35 +0600

    my changes

--- assets/js/borhan.js
+++ assets/js/borhan.js
@@ -30,7 +30,7 @@ var CategorySlider = new Swiper('.category-slider', {
 });
 
 
-// Custom js
+// toggle left profile sidebar
 function SidebarToggle() {
     var SidebarMenu = document.getElementById("LeftSidebar");
     if (SidebarMenu.style.display === "none") {
@@ -38,37 +38,57 @@ function SidebarToggle() {
     } else {
       SidebarMenu.style.display = "none";
     }
-  }   
+  }
 
+// toggle right cart sidebar
+function CartSidebarToggle() {
+  var CartSidebarMenu = document.getElementById("CartSidebar");
   
-  // Increase numbers
- function decreseNumber() {
-  const CartValue = document.getElementById('textBox');
-  alert(CartValue.value);
+  if (CartSidebarMenu.style.display === "none") {
+    CartSidebarMenu.style.display = "block";
+  } else {
+    CartSidebarMenu.style.display = "none";
+  }
 }
 
 
 
 
+// Click to margin top icon
+
+const ClickToTop = document.querySelector('.ClickTop');
+
+window.addEventListener('scroll', () => {
+
+    if (window.pageYOffset > 100) {
+        ClickToTop.classList.add("active");
+    } else {
+        ClickToTop.classList.remove("active");
+    }
+})
+
+ClickToTop.addEventListener("click", function(){
+    window.scrollTo(0, 0);
+});
 
 
+// Sticky menu
+window.onscroll = function() {myFunction()};
 
+var MainMenu = document.getElementById("MainMenu");
+var stickyMenu = MainMenu.offsetTop;
 
+function myFunction() {
 
+  if (window.pageYOffset >= stickyMenu) {
 
+    MainMenu.classList.add("stickyMenu")
+    document.querySelector('stickyMenu').style.zIndex = "1000";
 
+  } else {
 
+    MainMenu.classList.remove("stickyMenu");
 
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
